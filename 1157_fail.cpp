ac497084e7fbb3733925a097c27f67d6bdfbcef2
TRV449 <71203826+TRV449@users.noreply.github.com>
2022-02-16 23:59:03 +0900

    Create 1157_fail.cpp

---
+++ 1157_fail.cpp
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+class Program {
+    static void Main(string[] args) {
+      string str = Console.ReadLine();
+      str = str.ToLower();
+
+      int[] list = new int[26];
+      for(int i = 0; i < 26; i++)
+        list[i] = 0;
+      
+      for(int i = 0; i < str.Length; i++){
+        list[((int)str[i] - 97)]++;
+      }
+
+      int Max = list[0];
+      int maxind = 0;
+      int ifduple = 0;
+      for(int i = 1; i < 26; i++){
+        if(Max < list[i]){
+          Max = list[i];
+          maxind = i;
+        }
+        else if(Max == list[i]){
+          ifduple = 1;
+        }
+      }
+      if(ifduple == 1)
+        Console.WriteLine("?");
+      else
+        Console.WriteLine((char)(maxind+65));
+    }
+}
